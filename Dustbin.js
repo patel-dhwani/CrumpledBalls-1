dd10cc80a59bc5a7050adde754ffd40e8dc46b51
patel-dhwani <77050044+patel-dhwani@users.noreply.github.com>
2021-04-08 07:41:19 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,25 @@
+class dustbin
+{
+constructor(x,y,width,height)
+{
+    var option={
+        isStatic:true
+    }
+    this.dustbinObject=Bodies.rectangle(x,y,width,height,option);
+    World.add(world,this.dustbinObject);
+    this.width=width;
+    this.height=height;
+    
+    
+}
+
+display()
+{
+    fill("yellow");
+    noStroke();
+    
+    rectMode(CENTER);
+    rect(this.dustbinObject.position.x,this.dustbinObject.position.y,this.width,this.height);
+
+} 
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# PaperThrowChallenge!!
